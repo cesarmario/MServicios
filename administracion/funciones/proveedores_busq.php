f8bde2c20552ce869d772f605c67275caf0a5025
cesarmario <38296420+cesarmario@users.noreply.github.com>
2022-01-19 09:37:04 -0300

    Update proveedores_busq.php

--- administracion/funciones/proveedores_busq.php
+++ administracion/funciones/proveedores_busq.php
@@ -6,11 +6,11 @@
 <?PHP
 	
 	if (isset($_REQUEST['nom'])){$nom=$_REQUEST['nom'];}else{$nom="";};
-	if (isset($_REQUEST['rubro'])){$idrub=$_REQUEST['rubro'];}else{$idrub=0;};
-	if (isset($_REQUEST['espec'])){$idesp=$_REQUEST['espec'];}else{$idesp=0;};
-	echo "id rubro:".$idrub;
-	echo "<br>id espec:".$idesp;
-	if ($idrub==0 and $idesp==0){
+	if (isset($_REQUEST['rubro'])){$rubro=$_REQUEST['rubro'];}else{$rubro=0;};
+	if (isset($_REQUEST['espec'])){$espec=$_REQUEST['espec'];}else{$espec=0;};
+	echo "id rubro:".$rubro;
+	echo "<br>id espec:".$espec;
+	if ($rubro==0 and $espec==0){
 		$query="SELECT * FROM proveedores ORDER BY nombre ASC "; ?>
 		<script>
 			//alert("No hay filtros!");
@@ -18,20 +18,21 @@
 	<?PHP
 	} else {
 		
-		if ($idrub>0){
+		if ($rubro>0){
 			
-			$qrub="SELECT * FROM rubros WHERE id = $idrub";
+			$qrub="SELECT * FROM rubros WHERE id = $rubro";
 			$rstrub = mysqli_query($conexion, $qrub);
-			$rub=mysqli_fetch_assoc($rstrub);
-			$nrubro = upper($rub["descripcion"]);
-			
+			if ($rub=mysqli_fetch_assoc($rstrub)){
+			$nrubro = upper($rub["descripcion"]);			
 			echo "Busca Rubro: " . $rubro;
 			$query="SELECT * FROM proveedores WHERE upper(rubro1) LIKE '%$nrubro%'";			
-
+			}else {
+				echo "Error ID Rubro";
+			}
 		}
 		
-		if ($idesp!="0"){
-			$query="SELECT * FROM proveedores WHERE upper(especialidad1) LIKE '%$$idesp%'";
+		if ($espec>0){
+			$query="SELECT * FROM proveedores WHERE upper(especialidad1) LIKE '%$$espec%'";
 	
 		}
 	} 
